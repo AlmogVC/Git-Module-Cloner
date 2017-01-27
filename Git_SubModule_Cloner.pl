b1ae44dc2a56526fdab97c2de169d6f6630d43c8
AlmogVC <almogvc@gmail.com>
2017-01-27 11:09:03 +0200

    Update Git_SubModule_Cloner.pl

--- Git_SubModule_Cloner.pl
+++ Git_SubModule_Cloner.pl
@@ -11,8 +11,6 @@ open($file, '<', $filename)
 @arrFileLine = <$file>;
 close($file);
 
-print(scalar(@arrFileLine) / 3);
-
 for ($nLineIndex = 0; $nLineIndex  + 3 < scalar(@arrFileLine); $nLineIndex += 3)
 {
 	if($arrFileLine[$nLineIndex + 1] =~ m/path = (.*)\/repo\n/)
